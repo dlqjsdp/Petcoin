e585d20d56ea5d2d39f69627ce03cf3945d9ffb2
dlqjsdp <dlqjsdp224@gmail.com>
2025-09-02 14:19:11 +0900

    리액트 구조 변경, 로그인 화면 수정

--- frontend/src/admin/AdminApp.js
+++ frontend/src/admin/AdminApp.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import AdminDashboard from './admin/components/AdminDashboard'; // 올바른 경로
-import './App.css';
+import AdminDashboard from './components/AdminDashboard'; // 올바른 경로
+// import '../App.css';
+import './styles/AdminDashboard.css';
 
 function App() {
     return (
